7a6e90f57706c34c830551a0213138d3d9f0e51d
Arun Francis A <francisarun1999@gmail.com>
2024-06-18 12:07:45 +0530

    Init Commit

--- app/routes/main.routes.js
+++ app/routes/main.routes.js
@@ -11,20 +11,10 @@ router.post("/add-team/", Maincontroller.AddTeam);
 //result endpoint
 router.get("/process-result/", Maincontroller.ProcessResult);
 
-//maiden over endpoint
-// router.get("/maiden-result/", Maincontroller.MaidenResult);
-
 //team result endpoint
 router.get("/team-result/", Maincontroller.TeamResult);
 
 module.exports = router;
 
-// module.exports = app => {
-//     const router = require("express").Router();
-//     const Maincontroller = require("../controllers/main.controller.js");
-//     router.get("/getplayers", Maincontroller.GetPlayers);
-
-//     app.use('/api/main', router); 
-// };
 
     
